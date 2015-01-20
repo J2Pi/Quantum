9021a4c912f90675d2dd91db6fe20c809db33eae
Jonathan Tau <jtau@berkeley.edu>
2015-01-19 21:34:08 -0800

    Modified some urls.py

--- ToDoList/models.py
+++ ToDoList/models.py
@@ -3,16 +3,16 @@ from django.contrib.auth.models import User
 # Create your models here.
 class TaskList(models.Model):
 	user = models.ForeignKey(User)
-	tasks = models.OneToManyField('Task')
 
 class Task(models.Model):
 	task_title = models.CharField(max_length=200)
 	checkbox = models.BooleanField(default = False)
+	task_list = ForeignKey('TaskList')
 
 class TaskInfo(models.Model):
 	task_desc = models.CharField(max_length=200)
 	create_date = models.DateTimeField('date created')
 	due_date = models.DateTimeField('date due')
-
+	
 class CollaboratorList(model.Model):
 	collaborator = models.OneToManyField(User)
