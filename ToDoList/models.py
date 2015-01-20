65a0d788e73f54b168f5c359567b365f97d7c653
Jonathan Tau <jtau@berkeley.edu>
2015-01-19 19:41:18 -0800

    Added collaborator classes to todolist app

--- ToDoList/models.py
+++ ToDoList/models.py
@@ -1,6 +1,10 @@
 from django.db import models
-
+from django.contrib.auth.models import User
 # Create your models here.
+class TaskList(models.Model):
+	user = models.ForeignKey(User)
+	tasks = models.OneToManyField('Task')
+
 class Task(models.Model):
 	task_title = models.CharField(max_length=200)
 	checkbox = models.BooleanField(default = False)
@@ -9,4 +13,6 @@ class TaskInfo(models.Model):
 	task_desc = models.CharField(max_length=200)
 	create_date = models.DateTimeField('date created')
 	due_date = models.DateTimeField('date due')
-	#collaborator = User(constructor fields)
+
+class CollaboratorList(model.Model):
+	collaborator = models.OneToManyField(User)
